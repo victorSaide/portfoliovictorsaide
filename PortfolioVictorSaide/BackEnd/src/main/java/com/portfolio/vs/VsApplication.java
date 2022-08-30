61d9db4a70dcf880e499784b6da423d6b2db4edf
loki0101 <66391514+loki0101@users.noreply.github.com>
2022-08-30 14:43:29 -0300

    Se vincula exitosamente el back con el front; se crean las carpetas model y service

--- PortfolioVictorSaide/BackEnd/src/main/java/com/portfolio/vs/VsApplication.java
+++ PortfolioVictorSaide/BackEnd/src/main/java/com/portfolio/vs/VsApplication.java
@@ -8,6 +8,7 @@ public class VsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VsApplication.class, args);
+                System.out.println("La app corre bien");
 	}
 
 }
